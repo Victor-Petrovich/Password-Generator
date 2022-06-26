d3a1877eabf5b57697d1733a9a7e2e2779788ad2
victor <victor.petrovich1994@gmail.com>
2022-06-27 00:26:32 +0300

    Forms

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'generator/home.html', {'password':'hui43g6iu3o4'})
+    return render(request, 'generator/home.html')
 
-def eggs(request):
-    return HttpResponse('<h1>Eggs are so tasty</h1>')
+def password(request):
+    return render(request, 'generator/password.html')
 
